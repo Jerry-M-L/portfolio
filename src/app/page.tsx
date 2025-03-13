4d8f92b5a0f92f8a33b0947c9a13d685946ea7f8
Jerry-M-L <https://github.com/Jerry-M-L/p01-github-init>
2025-03-13 17:26:19 +0100

    nouvelle modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+import { useState } from "react";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          MBENDA-LECA Jerry
-      </main>
-      
+    <div>
+      <h1 className="text-4xl font-semibold">
+        Bringing<br />
+        the computer<br />
+        to life
+      </h1>
+
+      <Counter />
     </div>
   );
 }
+
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="pt-10">
+      <button className="mr-3 bg-gray-200 px-4 py-2 rounded-md" onClick={() => setCount(count + 1)}>+</button>
+      <button className="bg-gray-200 px-4 py-2 rounded-md" onClick={() => setCount(count - 1)}>-</button>
+      <div className="mt-4 text-4xl font-semibold">{count}</div>
+    </div>
+  )
+};
